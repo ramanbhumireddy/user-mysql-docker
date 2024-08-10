b212335f93ed3891f79eaee61488a276755b2583
ramanbhumireddy <raman.bhumireddy@gmail.com>
2024-08-10 15:59:03 +0530

    first commit

---
+++ src/main/java/com/linex/UserMysqlDockerApplication.java
@@ -0,0 +1,13 @@
+package com.linex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMysqlDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMysqlDockerApplication.class, args);
+	}
+
+}
